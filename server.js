2da627325cd417a293cf8359f18433d96acc627a
KhamidkhanM <HoshimalievHamidxonn@gmail.com>
2026-04-21 18:12:22 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,30 +4,26 @@ const app = express();
 const http = require("http");
 
 // 1. Kirish kodlari
-// 1.
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// 2. View engine va views papkasini sozlash
-// 3.
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-
 // 3. Routing
-// 4. Routing
-app.post("/create-item", function (req, res) {
-    console.log(req.body);
-    res.json({ test: "success" });
+app.get("/", function (req, res) {
+    res.render("harid", { reja: "Non, Sut, Tuxum, Yog'urt" });
 });
 
-app.get("/", function (req, res) {
-    res.send("harid");
+app.post("/create-item", function (req, res) {
+    console.log(req.body);
+    const newItem = req.body.item;
+    res.render("harid", { reja: `Yangi qo'shilgan: ${newItem}` });
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-    console.log(` The server is running successfully on port ${PORT}`);
+    console.log(`The server is running successfully on port ${PORT}`);
 });
