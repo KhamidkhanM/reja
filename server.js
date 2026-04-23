d181bbdbfea528e5a1e6a414acfec4e2db12bd80
KhamidkhanM <HoshimalievHamidxonn@gmail.com>
2026-04-24 05:46:23 +0900

    updated train.js

--- server.js
+++ server.js
@@ -2,18 +2,40 @@ console.log("Web serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
-// 1. Kirish kodlari
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
+
+// Middleware
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// View engine
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-// 3. Routing
+// Routes
 app.get("/", function (req, res) {
     res.render("harid", { reja: "Non, Sut, Tuxum, Yog'urt" });
+});
+
+app.get("/author", function (req, res) {
+    res.render("author", { user: user });
+});
+
+app.get("/doctor", function (req, res) {
+    res.render("doctor", { 
+        title: "Doctor Portfolio",
+        user: user 
+    });
 });
 
 app.post("/create-item", function (req, res) {
@@ -27,3 +49,5 @@ let PORT = 3000;
 server.listen(PORT, function () {
     console.log(`The server is running successfully on port ${PORT}`);
 });
+
+require('./train');
--- train.js
+++ train.js
@@ -1,1 +1,94 @@
-console.log('Train area')
+console.log("Jack Ma maslahatlari");
+const list = [
+  "yahshi talaba boling", // 0-20
+  "togri boshliq tanlang va koproq hato qiling", // 20-30
+  "uzingizga ishlashingizni boshlang", // 30-40
+  "siz kuchli bolgan narsalarni qiling", // 40-50
+  "yoshlarga investitsiya qiling", // 50-60
+  "endi dam oling, foydasi yoq endi", // 60
+];
+
+function maslahatBering(a, callback) {
+  if (typeof a !== "number") callback("Please, insert a number", null);
+  else if (a<=20) callback(null, list[0]);
+  else if (a>20 && a <= 30) callback(null, list[1]);
+  else if (a>30 && a <= 40) callback(null, list[2]);
+  else if (a>40 && a <= 50) callback(null, list[3]);
+  else if (a>50 && a <= 60) callback(null, list[4]);
+  else {
+    // callback(null, list[5]);
+    setInterval( function() {
+        callback(null, list[5]);
+    }, 1000 );
+  } 
+}
+
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("Please, insert a number");
+//   else if (a<=20) return (null, list[0]);
+//   else if (a>20 && a <= 30) return (null, list[1]);
+//   else if (a>30 && a <= 40) return (null, list[2]);
+//   else if (a>40 && a <= 50) return (null, list[3]);
+//   else if (a>50 && a <= 60) return (null, list[4]);
+//   else {
+//     return new Promise( (resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(list[5]);
+//         }, 5000 );
+    // callback(null, list[5]);
+    // setTimeout( function() {
+    //     callback(null, list[5]);
+    // }, 5000 );
+//     });
+//   } 
+// }
+
+console.log("Passed here 0")
+maslahatBering(70, function (err, data) {
+  if (err) console.log("ERROR:", err);
+  else 
+    console.log("Javob:", data);
+});
+console.log("Passed here 1")
+
+// console.log("Passed here 0")
+// maslahatBering(25)
+// .then( data => {
+//     maslahatBering(35)
+// .then( data => {
+//     maslahatBering(45)
+// .then( data => {
+//     console.log("Javob:", data);
+// })
+// .catch( err => {
+//     console.log("ERROR:", err);
+// });
+// console.log("Passed here 1")
+//     console.log("Javob:", data);
+// })
+// .catch( err => {
+//     console.log("ERROR:", err);
+// });
+// console.log("Passed here 1")
+//     console.log("Javob:", data);
+// })
+// .catch( err => {
+//     console.log("ERROR:", err);
+// });
+// console.log("Passed here 1")
+
+
+//then/catch bilan ishlatish
+// async function run() {
+//     let data = await maslahatBering(25);
+//     console.log("Javob:", data);
+//     data = await maslahatBering(35);
+//     console.log("Javob:", data);
+//     data = await maslahatBering(65);
+//     console.log("Javob:", data);
+//     data = await maslahatBering(55);
+//     console.log("Javob:", data);
+//     data = await maslahatBering(45);
+//     console.log("Javob:", data);
+// }
+// run();
