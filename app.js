ff3803345b3efde24a4e558736ccf6381bf089eb
KhamidkhanM <HoshimalievHamidxonn@gmail.com>
2026-04-27 21:41:47 +0900

    fix: MongoDB added

---
+++ app.js
@@ -0,0 +1,52 @@
+console.log("Server is starting...");
+
+const express = require("express");
+const app = express();
+
+const fs = require("fs");
+
+// MongoDB connect 
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
+
+let rejaList = [];
+
+// 1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing code
+app.post("/create-item", (req, res) => {
+    const newItem = req.body.item;
+    rejaList.push(newItem);
+    res.render("reja", { user: user, rejaList: rejaList });
+});
+
+app.post("/delete-all", (req, res) => {
+    rejaList = [];
+    res.render("reja", { user: user, rejaList: rejaList });
+});
+
+app.get("/", function (req, res) {
+    res.render("reja", { user: user, rejaList: rejaList });
+});
+
+app.post("/delete-item", (req, res) => {
+    const item = req.body.item;
+    rejaList = rejaList.filter(i => i !== item);
+    res.render("reja", { user: user, rejaList: rejaList });
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,54 +1,23 @@
-console.log("Server is starting...");
-
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if (err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data);
+let db;
+const connectionString = "mongodb://atlas-sql-69ef1d36c01470813b1edb51-yxkepp.a.query.mongodb.net/Reja?ssl=true&authSource=admin";
+
+mongodb.connect(connectionString, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+        console.log("MongoDB connected successfully");
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function () {
+            console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+        });
     }
-});
-
-let rejaList = [];
-
-// 1: Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// 3: Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 Routing code
-app.post("/create-item", (req, res) => {
-    const newItem = req.body.item;
-    rejaList.push(newItem);
-    res.render("reja", { user: user, rejaList: rejaList });
-});
-
-app.post("/delete-all", (req, res) => {
-    rejaList = [];
-    res.render("reja", { user: user, rejaList: rejaList });
-});
-
-app.get("/", function (req, res) {
-    res.render("reja", { user: user, rejaList: rejaList });
-});
-
-app.post("/delete-item", (req, res) => {
-    const item = req.body.item;
-    rejaList = rejaList.filter(i => i !== item);
-    res.render("reja", { user: user, rejaList: rejaList });
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
 });
