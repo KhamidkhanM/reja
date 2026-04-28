048659f20268158ee4cd8d3b9c7bbd91882f8dfb
KhamidkhanM <HoshimalievHamidxonn@gmail.com>
2026-04-28 17:54:33 +0900

    fix: added task B

--- train.js
+++ train.js
@@ -1,27 +1,63 @@
-console.log("Jack Ma maslahatlari");
-const list = [
-  "yahshi talaba boling", // 0-20
-  "togri boshliq tanlang va koproq hato qiling", // 20-30
-  "uzingizga ishlashingizni boshlang", // 30-40
-  "siz kuchli bolgan narsalarni qiling", // 40-50
-  "yoshlarga investitsiya qiling", // 50-60
-  "endi dam oling, foydasi yoq endi", // 60
-];
-
-function maslahatBering(a, callback) {
-  if (typeof a !== "number") callback("Please, insert a number", null);
-  else if (a<=20) callback(null, list[0]);
-  else if (a>20 && a <= 30) callback(null, list[1]);
-  else if (a>30 && a <= 40) callback(null, list[2]);
-  else if (a>40 && a <= 50) callback(null, list[3]);
-  else if (a>50 && a <= 60) callback(null, list[4]);
-  else {
-    // callback(null, list[5]);
-    setInterval( function() {
-        callback(null, list[5]);
-    }, 1000 );
-  } 
+//Task B
+function countDigits(str) {
+    let count = 0;
+    
+    for (let i = 0; i < str.length; i++) {
+        if (!isNaN(str[i])) {
+            count++;
+        }
+    }
+    
+    return count;
 }
+
+console.log(countDigits("ad2a54y79wet0sfgb9"));
+
+
+
+
+//Task A
+
+// function countLetter(harf, soz) {
+//   let count = 0;
+  
+//   for (let i = 0; i < soz.length; i++) {
+//     if (soz[i] === harf) {
+//       count++;
+//     }
+//   }
+  
+//   return count;
+// }
+
+// console.log(countLetter("a", "banana"));  
+
+
+
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yahshi talaba boling", // 0-20
+//   "togri boshliq tanlang va koproq hato qiling", // 20-30
+//   "uzingizga ishlashingizni boshlang", // 30-40
+//   "siz kuchli bolgan narsalarni qiling", // 40-50
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling, foydasi yoq endi", // 60
+// ];
+
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("Please, insert a number", null);
+//   else if (a<=20) callback(null, list[0]);
+//   else if (a>20 && a <= 30) callback(null, list[1]);
+//   else if (a>30 && a <= 40) callback(null, list[2]);
+//   else if (a>40 && a <= 50) callback(null, list[3]);
+//   else if (a>50 && a <= 60) callback(null, list[4]);
+//   else {
+//     // callback(null, list[5]);
+//     setInterval( function() {
+//         callback(null, list[5]);
+//     }, 1000 );
+//   } 
+// }
 
 // async function maslahatBering(a) {
 //   if (typeof a !== "number") throw new Error("Please, insert a number");
@@ -43,13 +79,13 @@ function maslahatBering(a, callback) {
 //   } 
 // }
 
-console.log("Passed here 0")
-maslahatBering(70, function (err, data) {
-  if (err) console.log("ERROR:", err);
-  else 
-    console.log("Javob:", data);
-});
-console.log("Passed here 1")
+// console.log("Passed here 0")
+// maslahatBering(70, function (err, data) {
+//   if (err) console.log("ERROR:", err);
+//   else 
+//     console.log("Javob:", data);
+// });
+// console.log("Passed here 1")
 
 // console.log("Passed here 0")
 // maslahatBering(25)
@@ -97,17 +133,4 @@ console.log("Passed here 1")
 
 
 
-// function countLetter(harf, soz) {
-//   let count = 0;
-  
-//   for (let i = 0; i < soz.length; i++) {
-//     if (soz[i] === harf) {
-//       count++;
-//     }
-//   }
-  
-//   return count;
-// }
-
-
-// console.log(countLetter("a", "banana"));   
+ 
